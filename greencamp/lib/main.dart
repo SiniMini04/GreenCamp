23a13d59338417501e66488378e1cbd062577829
SiniSim <63308908+SiniMini04@users.noreply.github.com>
2023-03-30 16:26:22 +0200

    Start:Bild:01
    Das Bild des CampingPlatzes wurde als Background Image hinzugefügt.

--- greencamp/lib/main.dart
+++ greencamp/lib/main.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  runApp(const MaterialApp(
       debugShowCheckedModeBanner: false,
-    );
-  }
+      home: Scaffold(
+        body: Center(
+          child: Image(image: AssetImage('assets/Campingplatz_Layout.jpg')),
+        ),
+      )));
 }
