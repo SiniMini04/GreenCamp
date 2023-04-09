ca0aace90b22d9910f0fa03551f4295434d257e8
SiniSim <63308908+SiniMini04@users.noreply.github.com>
2023-04-09 18:06:19 +0200

    Start:Icons_hinzufügen:01
    Ein Icon wurde als IconButton hinzugefügt an der richtigen Position

--- greencamp/lib/main.dart
+++ greencamp/lib/main.dart
@@ -1,11 +1,35 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Image(image: AssetImage('assets/Campingplatz_Layout.jpg')),
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Container(
+      //Adding Background Image
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/Campingplatz_Layout.jpg'),
+              fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+                bottom: 615,
+                left: 325,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.fiber_manual_record),
+                  color: Colors.red,
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                ))
+          ],
         ),
-      )));
+      ),
+    ),
+  ));
 }
