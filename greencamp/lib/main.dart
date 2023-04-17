c2388e08c5c7f101dfa5a267c8ab52a6e82b2277
SiniSim <63308908+SiniMini04@users.noreply.github.com>
2023-04-17 20:08:11 +0200

    Buttons responsive auf Bildschirm:Buttons responsive auf Bildschirm:03
    Made the Buttons responsive. Some of the buttons don't fit anymore and need to be changed again!!!!

--- greencamp/lib/main.dart
+++ greencamp/lib/main.dart
@@ -5,34 +5,44 @@ void main() {
   int campingPlaceKey = 0;
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Container(
-      //Adding Background Image
-      decoration: const BoxDecoration(
+    home: Builder(
+      builder: (context) => Container(
+        decoration: const BoxDecoration(
           image: DecorationImage(
-              image: AssetImage('assets/Campingplatz_Layout.jpg'),
-              fit: BoxFit.cover)),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Stack(
-          alignment: Alignment.center,
-          children: [
-            //Adding IconButtons to Immage on every single Campinggrid
-            for (final position in positions)
-              Positioned(
-                bottom: position['bottom'],
-                left: position['left'],
-                right: position['right'],
-                child: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.fiber_manual_record),
-                  color: Colors.green,
-                  splashColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                  hoverColor: Colors.transparent,
-                  key: ValueKey(campingPlaceKey++),
+            image: AssetImage('assets/Campingplatz_Layout.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Stack(
+            alignment: Alignment.center,
+            children: [
+              // Adding IconButtons to Image on every single Camping grid
+              for (final position in positions)
+                Positioned(
+                  bottom: MediaQuery.of(context).size.height *
+                      (position['bottom'] ?? 0),
+                  left: position.containsKey('right')
+                      ? null
+                      : MediaQuery.of(context).size.width *
+                          (position['left'] ?? 0),
+                  right: position.containsKey('right')
+                      ? MediaQuery.of(context).size.width *
+                          (position['right'] ?? 0)
+                      : null,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.fiber_manual_record),
+                    color: Colors.green,
+                    splashColor: Colors.transparent,
+                    highlightColor: Colors.transparent,
+                    hoverColor: Colors.transparent,
+                    key: ValueKey(campingPlaceKey++),
+                  ),
                 ),
-              ),
-          ],
+            ],
+          ),
         ),
       ),
     ),
