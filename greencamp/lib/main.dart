c7ac54562ca258c2d8736bace040d9878c8088b5
SiniSim <63308908+SiniMini04@users.noreply.github.com>
2023-04-20 10:07:55 +0200

    Buttons responsive auf Bildschirm:Buttons responsive auf Bildschirm:06
    Optimized the code

--- greencamp/lib/main.dart
+++ greencamp/lib/main.dart
@@ -2,49 +2,49 @@ import 'package:flutter/material.dart';
 import 'positions.dart';
 
 void main() {
-  int campingPlaceKey = 0;
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Builder(
-      builder: (context) => Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/Campingplatz_Layout.jpg'),
-            fit: BoxFit.cover,
+      builder: (context) {
+        final mediaSize = MediaQuery.of(context).size;
+        return Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/Campingplatz_Layout.jpg'),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Stack(
-            alignment: Alignment.center,
-            children: [
-              // Adding IconButtons to Image on every single Camping grid
-              for (final position in positions)
-                Positioned(
-                  bottom: MediaQuery.of(context).size.height *
-                      (position['bottom'] ?? 0),
-                  left: position.containsKey('right')
-                      ? null
-                      : MediaQuery.of(context).size.width *
-                          (position['left'] ?? 0),
-                  right: position.containsKey('right')
-                      ? MediaQuery.of(context).size.width *
-                          (position['right'] ?? 0)
-                      : null,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.fiber_manual_record),
-                    color: Colors.green,
-                    splashColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    hoverColor: Colors.transparent,
-                    key: ValueKey(campingPlaceKey++),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Stack(
+              alignment: Alignment.center,
+              children: [
+                // Adding IconButtons to Image on every single Camping grid
+                for (var index = 0; index < positions.length; index++)
+                  Positioned(
+                    bottom:
+                        mediaSize.height * (positions[index]['bottom'] ?? 0),
+                    left: positions[index].containsKey('right')
+                        ? null
+                        : mediaSize.width * (positions[index]['left'] ?? 0),
+                    right: positions[index].containsKey('right')
+                        ? mediaSize.width * (positions[index]['right'] ?? 0)
+                        : null,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.fiber_manual_record),
+                      color: Colors.green,
+                      splashColor: Colors.transparent,
+                      highlightColor: Colors.transparent,
+                      hoverColor: Colors.transparent,
+                      key: ValueKey(index),
+                    ),
                   ),
-                ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     ),
   ));
 }
