d9dc3b9c28db856b0e84ad5a4403720d77c648f0
evelinbaan <evelin.baan@stud.kftg.ch>
2023-06-19 20:21:25 +0200

    Rahmen-um-den-Blitz:Rahmen-um-den-Blitz:02
    Rahmen um den Blitz erstellt

--- greencamp/lib/main.dart
+++ greencamp/lib/main.dart
@@ -216,61 +216,61 @@ class _MyAppState extends State<MyApp> {
                     ),
                   ),
                   body: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      // Adding IconButtons to Image on every single Camping grid
-                      for (var index = 0; index < positions.length; index++)
-                        Positioned(
-                          bottom: mediaSize.height *
-                              (positions[index]['bottom'] ?? 0),
-                          left: positions[index].containsKey('right')
-                              ? null
-                              : mediaSize.width *
-                                  (positions[index]['left'] ?? 0),
-                          right: positions[index].containsKey('right')
-                              ? mediaSize.width *
-                                  (positions[index]['right'] ?? 0)
-                              : null,
-                          child: Stack(
-                            children: [
-                              _isLoading
-                                  ? CircularProgressIndicator()
-                                  : IconButton(
-                                      onPressed: () async {
-                                        if (await checkWhichPopUp(index)) {
-                                          gridInfoAfterInsert(
-                                              context, index, shownDate);
-                                        } else {
-                                          positioninfos(
-                                              context, index, shownDate);
-                                        }
-                                      },
-                                      icon:
-                                          const Icon(Icons.fiber_manual_record),
-                                      color: _isButtonFree[index]
-                                          ? Colors.red
-                                          : Colors.green,
-                                      splashColor: Colors.transparent,
-                                      highlightColor: Colors.transparent,
-                                      hoverColor: Colors.transparent,
-                                      key: ValueKey(index),
-                                    ),
-                              if (positions[index]['electricityConnection'] ==
-                                  true)
-                                Positioned.fill(
-                                  child: Container(
-                                    child: Icon(
-                                      Icons.bolt,
-                                      color: Colors.yellow,
-                                      size: 20.0,
-                                    ),
-                                  ),
-                                ),
-                            ],
-                          ),
-                        ),
-                    ],
+  alignment: Alignment.center,
+  children: [
+    // Adding IconButtons to Image on every single Camping grid
+    for (var index = 0; index < positions.length; index++)
+      Positioned(
+        bottom: mediaSize.height * (positions[index]['bottom'] ?? 0),
+        left: positions[index].containsKey('right')
+            ? null
+            : mediaSize.width * (positions[index]['left'] ?? 0),
+        right: positions[index].containsKey('right')
+            ? mediaSize.width * (positions[index]['right'] ?? 0)
+            : null,
+        child: Stack(
+          children: [
+            _isLoading
+                ? CircularProgressIndicator()
+                : IconButton(
+                    onPressed: () async {
+                      if (await checkWhichPopUp(index)) {
+                        gridInfoAfterInsert(context, index, shownDate);
+                      } else {
+                        positioninfos(context, index, shownDate);
+                      }
+                    },
+                    icon: const Icon(Icons.fiber_manual_record),
+                    color: _isButtonFree[index] ? Colors.red : Colors.green,
+                    splashColor: Colors.transparent,
+                    highlightColor: Colors.transparent,
+                    hoverColor: Colors.transparent,
+                    key: ValueKey(index),
                   ),
+            if (positions[index]['electricityConnection'] == true)
+              Positioned(
+                bottom: 0, // Adjust the position of the yellow circle as needed
+                child: Container(
+                  width: 35.0, // Adjust the size of the container as needed
+                  height: 35.0, // Adjust the size of the container as needed
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.yellow,
+                  ),
+                  child: Icon(
+                    Icons.bolt,
+                    color: Colors.black,
+                    size: 20.0, // Adjust the size of the bolt icon as needed
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+  ],
+),
+
+
                 ),
               ),
               Positioned(
